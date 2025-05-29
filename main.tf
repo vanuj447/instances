93f4bb1726b3243b8de2461d5a030fc923d1ccb0
vanuj447 <vanuj447@gmail.com>
2025-05-29 17:15:10 +0530

    yes

--- main.tf
+++ main.tf
@@ -9,8 +9,10 @@ region = "us-east-1"
 
 resource "aws_instance" "my_vm" {
 ami = "ami-084568db4383264d4"
-subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
 instance_type = "t3.micro"
+subnet_id =data.terraform_remote_state.network_details.outputs.my_subnet
+key_name = data.terraform_remote_state.network_details.outputs.my_aws_key_pair
+
 tags = {
 Name = "student.3-vm1"
 }
--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
-output "public_ip"{
- value = aws_instance.my_vm.public_ip
+output "public_ip" {
+value = aws_instance.my_vm.public_ip
 }
+
+
+
+
+
+
+
+
