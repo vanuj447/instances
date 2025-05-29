4ccc2dc45e2a95460c5eab6fe069afe00ad98f98
vanuj447 <vanuj447@gmail.com>
2025-05-29 14:37:46 +0530

    end of lab

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ key = "student.3-network-state"
 region = "us-east-1"
 }
 }
+
+resource "aws_instance" "my_vm" {
+ami = "ami-084568db4383264d4"
+subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
+instance_type = "t3.micro"
+tags = {
+Name = "student.3-vm1"
+}
+}
