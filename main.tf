bf58db939ec19ec7d8c0ccd9ae4ee3d8d44c9240
vanuj447 <vanuj447@gmail.com>
2025-05-29 17:20:36 +0530

    end of ssh keys and sg labs

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ ami = "ami-084568db4383264d4"
 instance_type = "t3.micro"
 subnet_id =data.terraform_remote_state.network_details.outputs.my_subnet
 key_name = data.terraform_remote_state.network_details.outputs.my_aws_key_pair
-
+vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id_array
 tags = {
 Name = "student.3-vm1"
 }
