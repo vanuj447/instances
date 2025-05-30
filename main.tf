d7d9c631a0df2664743f34df0b4ba82d9870ce2b
vanuj447 <vanuj447@gmail.com>
2025-05-30 14:05:26 +0530

    end of lab launching multiples VMs

--- main.tf
+++ main.tf
@@ -19,3 +19,17 @@ tags = {
 Name = "student.3-vm1"
 }
 }
+
+
+module "loadbalancer" {
+source = "./modules/linux_node"
+ami = var.ami
+instance_count = "0"
+instance_type = var.instance_type
+key_name = data.terraform_remote_state.network_details.outputs.key_name
+subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
+vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id_array
+tags = {
+Name = "student.3-loadbalancer-vm"
+}
+}
--- output.tf
+++ output.tf
@@ -2,6 +2,9 @@ output "public_ip" {
 value = module.webserver.*.public_ip
 }
 
+output "loadbalancer_vm_public_ip" {
+value = module.loadbalancer.*.public_ip
+}
 
 
 
